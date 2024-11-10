085451a4b58ea0bd750f927e8b91991b0295143a
sabir meskin <sabirmeskin@gmail.com>
2024-11-10 12:12:00 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+    <h1 className='text-green-400'>hello world</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
